758d9d913ca4c63c011e7845e1dc08f6ae45816a
lor2ex <koreika@gmail.com>
2026-03-07 16:09:18 +0100

    added 3 admin classes

--- my_app/admin.py
+++ my_app/admin.py
@@ -2,7 +2,82 @@ from django.contrib import admin
 
 from my_app.models import Task, SubTask, Category
 
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "status",
+        "created_at",
+        "deadline"
+    ]
 
-admin.site.register(Task)
-admin.site.register(SubTask)
-admin.site.register(Category)
+    search_fields = [
+        "title",
+    ]
+
+    list_filter = [
+        "created_at",
+        "deadline"
+    ]
+
+    list_editable = [
+        "status"
+    ]
+
+    list_per_page = 10
+
+
+
+@admin.register(SubTask)
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "status",
+        "created_at",
+        "deadline"
+    ]
+
+    search_fields = [
+        "title",
+    ]
+
+    list_filter = [
+        "created_at",
+        "deadline"
+    ]
+
+    list_editable = [
+        "status"
+    ]
+
+    list_per_page = 10
+
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "name"
+    ]
+
+    list_display_links = [
+        "id"
+    ]
+
+    search_fields = [
+        "name",
+    ]
+
+    list_filter = [
+        "id",
+        "name"
+    ]
+
+    list_editable = [
+        "name"
+    ]
+
+    list_per_page = 10
+
+
